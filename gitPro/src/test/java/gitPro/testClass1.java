056d6082bdd38a1b919130a687e2675e630deb06
MAHESH PATODKAR <MAHESH PATODKAR@VIVEK>
2022-11-15 20:39:17 +0530

    First Commit

---
+++ gitPro/src/test/java/gitPro/testClass1.java
@@ -0,0 +1,18 @@
+package gitPro;
+
+public class testClass1 
+{
+  public static void main(String[] args)
+  {
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+	System.out.println("TC01");
+   }
+}
